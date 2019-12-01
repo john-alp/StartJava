1ac158346a3770a38445a5b61dab3939006fc5ed
john-alp <57313574+john-alp@users.noreply.github.com>
2019-12-01 09:03:23 +0300

    Первые шаги, после курса - Джава для начинающих

---
+++ src/Loops.java
@@ -0,0 +1,12 @@
+public class Loops {
+    public static void main(String[] ars){
+        int a = 0;
+        int b = 3;
+        System.out.println("Работает цикл while, " + a + " меньше " + b);
+        while (a < b){       // Kак и все операторы, пишется с маленькой буквы
+                             // Работает, пока значение true
+            System.out.println(a + " Меньше " + b);
+            a +=1;
+        }
+    }
+}
