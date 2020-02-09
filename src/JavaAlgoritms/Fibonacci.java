d8692721c2eca7fe09c9aff979a95e34f9acd6b2
john-alp <57313574+john-alp@users.noreply.github.com>
2020-02-09 20:13:29 +0300

    09.02.2020 Next curs Java

---
+++ src/JavaAlgoritms/Fibonacci.java
@@ -0,0 +1,19 @@
+package JavaAlgoritms;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        System.out.println(fib(10));
+
+    }
+    //Эффективный метод
+    public static long fib (int n) {
+        long[] arr = new long[n+1];
+        arr [0] = 0;
+        arr [1] = 1;
+        for (int i=2; i<=n; i++){
+            System.out.println();
+            arr [i] = arr[i-1] + arr[i-2];
+        }
+    return arr[n];
+    }
+}
